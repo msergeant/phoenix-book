12456bfd53554ecd3c2f0c93c63bb294248e0666
Mike Sergeant <sergeant.mj@gmail.com>
2018-02-16 22:39:00 -0500

    All good to page 142

--- lib/storex/sales/sales.ex
+++ lib/storex/sales/sales.ex
@@ -17,4 +17,52 @@ defmodule Storex.Sales do
   def get_cart!(id) do
     Repo.get(Cart, id)
   end
+
+  def add_book_to_cart(book, cart) do
+    line_item = Repo.get_by(LineItem, book_id: book.id, cart_id: cart.id)
+
+    if line_item do
+      line_item
+      |> LineItem.changeset(%{quantity: line_item.quantity + 1})
+      |> Repo.update()
+    else
+      %LineItem{book_id: book.id, cart_id: cart.id}
+      |> LineItem.changeset(%{quantity: 1})
+      |> Repo.insert()
+    end
+  end
+
+  def remove_book_from_cart(book, cart) do
+    line_item = Repo.get_by!(LineItem, book_id: book.id, cart_id: cart.id)
+
+    if line_item.quantity > 1 do
+      line_item
+      |> LineItem.changeset(%{quantity: line_item.quantity - 1})
+      |> Repo.update()
+    else
+      Repo.delete!(line_item)
+    end
+  end
+
+  def list_line_items(cart) do
+    LineItem
+    |> where(cart_id: ^cart.id)
+    |> preload(:book)
+    |> Repo.all()
+  end
+
+  def line_items_quantity_count(line_items) do
+    Enum.reduce(line_items, 0, fn(line_item, acc) ->
+      acc + line_item.quantity
+    end)
+  end
+
+  def line_items_total_price(line_items) do
+    Enum.reduce(line_items, Decimal.new("0"), fn(line_item, acc) ->
+      quantity = Decimal.new(line_item.quantity)
+      total_price = Decimal.mult(line_item.book.price, quantity)
+
+      Decimal.add(acc, total_price)
+    end)
+  end
 end
